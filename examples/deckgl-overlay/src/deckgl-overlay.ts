42a78b82dd3cb9591a07fa70e18a390a1eb21e64
Martin Schuhfuss <m.schuhfuss@gmail.com>
2023-09-26 13:57:38 +0200

    docs: add example for interleaved deck.gl overlays (#7)

---
+++ examples/deckgl-overlay/src/deckgl-overlay.ts
@@ -0,0 +1,31 @@
+import {useMap} from '@vis.gl/react-google-maps';
+import {useEffect, useMemo} from 'react';
+
+import {GoogleMapsOverlay} from '@deck.gl/google-maps/typed';
+
+import type {LayersList} from '@deck.gl/core/typed';
+
+export type DeckglOverlayProps = {layers?: LayersList};
+
+/**
+ * A very simple implementation of a component that renders a list of deck.gl layers
+ * via the GoogleMapsOverlay into the <Map> component containing it.
+ */
+export const DeckGlOverlay = ({layers}: DeckglOverlayProps) => {
+  // the GoogleMapsOverlay can persist throughout the lifetime of the DeckGlOverlay
+  const deck = useMemo(() => new GoogleMapsOverlay({interleaved: true}), []);
+
+  // add the overlay to the map once the map is available
+  const map = useMap();
+  useEffect(() => {
+    deck.setMap(map);
+  }, [map]);
+
+  // whenever the rendered data changes, the layers will be updated
+  useEffect(() => {
+    deck.setProps({layers});
+  }, [layers]);
+
+  // no dom rendered by this component
+  return null;
+};
