57fb1a14a22754f3338f3356f312721a22fb50cf
Martin Schuhfuss <m.schuhfuss@gmail.com>
2023-10-04 14:43:03 +0200

    docs: fix standalone examples
     - add minimal stylesheet so control-panel works
     - alias source version of package to support debugging use-cases

--- examples/_template/vite.config.js
+++ examples/_template/vite.config.js
@@ -1,5 +1,5 @@
-/* @ts-check */
 import {defineConfig, loadEnv} from 'vite';
+import {resolve} from 'node:path';
 
 export default defineConfig(({mode}) => {
   const {GOOGLE_MAPS_API_KEY} = loadEnv(mode, process.cwd(), '');
@@ -7,6 +7,11 @@ export default defineConfig(({mode}) => {
   return {
     define: {
       'process.env.GOOGLE_MAPS_API_KEY': JSON.stringify(GOOGLE_MAPS_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@vis.gl/react-google-maps': resolve('../../src/index.ts')
+      }
     }
   };
 });
