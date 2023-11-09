ed458ee415a6bcd28c67f5a8313c0e36d6969558
Mauricio Robayo <rfmajo@gmail.com>
2023-11-09 10:05:11 -0500

    use object const instead of enum

--- src/components/map-control.tsx
+++ src/components/map-control.tsx
@@ -12,37 +12,39 @@ type MapControlProps = PropsWithChildren<{
  * Copy of the `google.maps.ControlPosition` constants.
  * They have to be duplicated here since we can't wait for the maps API to load to be able to use them.
  */
-export enum ControlPosition {
-  TOP_LEFT = 1,
-  TOP_CENTER = 2,
-  TOP = 2,
-  TOP_RIGHT = 3,
-  LEFT_CENTER = 4,
-  LEFT_TOP = 5,
-  LEFT = 5,
-  LEFT_BOTTOM = 6,
-  RIGHT_TOP = 7,
-  RIGHT = 7,
-  RIGHT_CENTER = 8,
-  RIGHT_BOTTOM = 9,
-  BOTTOM_LEFT = 10,
-  BOTTOM_CENTER = 11,
-  BOTTOM = 11,
-  BOTTOM_RIGHT = 12,
-  CENTER = 13,
-  BLOCK_START_INLINE_START = 14,
-  BLOCK_START_INLINE_CENTER = 15,
-  BLOCK_START_INLINE_END = 16,
-  INLINE_START_BLOCK_CENTER = 17,
-  INLINE_START_BLOCK_START = 18,
-  INLINE_START_BLOCK_END = 19,
-  INLINE_END_BLOCK_START = 20,
-  INLINE_END_BLOCK_CENTER = 21,
-  INLINE_END_BLOCK_END = 22,
-  BLOCK_END_INLINE_START = 23,
-  BLOCK_END_INLINE_CENTER = 24,
-  BLOCK_END_INLINE_END = 25
-}
+export const ControlPosition = {
+  TOP_LEFT: 1,
+  TOP_CENTER: 2,
+  TOP: 2,
+  TOP_RIGHT: 3,
+  LEFT_CENTER: 4,
+  LEFT_TOP: 5,
+  LEFT: 5,
+  LEFT_BOTTOM: 6,
+  RIGHT_TOP: 7,
+  RIGHT: 7,
+  RIGHT_CENTER: 8,
+  RIGHT_BOTTOM: 9,
+  BOTTOM_LEFT: 10,
+  BOTTOM_CENTER: 11,
+  BOTTOM: 11,
+  BOTTOM_RIGHT: 12,
+  CENTER: 13,
+  BLOCK_START_INLINE_START: 14,
+  BLOCK_START_INLINE_CENTER: 15,
+  BLOCK_START_INLINE_END: 16,
+  INLINE_START_BLOCK_CENTER: 17,
+  INLINE_START_BLOCK_START: 18,
+  INLINE_START_BLOCK_END: 19,
+  INLINE_END_BLOCK_START: 20,
+  INLINE_END_BLOCK_CENTER: 21,
+  INLINE_END_BLOCK_END: 22,
+  BLOCK_END_INLINE_START: 23,
+  BLOCK_END_INLINE_CENTER: 24,
+  BLOCK_END_INLINE_END: 25
+} as const;
+export type ControlPosition =
+  (typeof ControlPosition)[keyof typeof ControlPosition];
 
 export const MapControl = ({children, position}: MapControlProps) => {
   const controlContainer = useMemo(() => document.createElement('div'), []);
