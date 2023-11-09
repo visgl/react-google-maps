92a5beb3821adccdffceb27185f369b2c9c0471f
Martin Schuhfuss <m.schuhfuss@gmail.com>
2023-11-09 22:36:07 +0100

    chore: fix glob pattern in .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,7 @@ module.exports = {
       }
     },
     {
-      files: ['./{src,examples}/*.{ts,mts,cts,tsx}'],
+      files: ['./{src,examples}/**/*.{ts,mts,cts,tsx}'],
       rules: {
         // Some of JS rules don't always work correctly in TS and
         // hence need to be reimported as TS rules
