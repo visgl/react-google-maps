4d87ae2598dbd5c244168b51cb8db4cac86938e7
Martin Schuhfuss <m.schuhfuss@gmail.com>
2023-11-11 13:43:52 +0100

    fix: add map camera state tracking
    Camera state tracking allows the Map component to avoid a feedback-loop when camera values published by the map are fed back into the Map component props.
    
    I measured round-trip times (dispatching an event into the react-application and receiving them back as props) of around 4-5ms. However, there are cases where this roundtrip crosses the frame-boundary, and times more like 20ms can be observed. When this happens, stuttering, janky animations and suboptimal interactions will happen.

--- src/components/map/use-map-options.ts
+++ src/components/map/use-map-options.ts
@@ -1,13 +1,32 @@
 import {useEffect, useLayoutEffect} from 'react';
 import {MapProps} from '@vis.gl/react-google-maps';
+import {InternalCameraStateRef} from './use-internal-camera-state';
+import {isLatLngLiteral} from '../../libraries/is-lat-lng-literal';
 
 /**
- * Internal hook to update the map-options and view-parameters when
+ * Internal hook to update the map-options and camera parameters when
  * props are changed.
+ *
+ * @param map the map instance
+ * @param cameraStateRef stores the last values seen during dispatch into the
+ * react-application in useMapEvents(). We can safely assume that we
+ * don't need to feed these values back into the map.
+ * @param mapProps the props to update the map-instance with
  * @internal
  */
-export function useMapOptions(map: google.maps.Map | null, mapProps: MapProps) {
-  const {center, zoom, heading, tilt, ...mapOptions} = mapProps;
+export function useMapOptions(
+  map: google.maps.Map | null,
+  cameraStateRef: InternalCameraStateRef,
+  mapProps: MapProps
+) {
+  const {center: rawCenter, zoom, heading, tilt, ...mapOptions} = mapProps;
+  const center = rawCenter
+    ? isLatLngLiteral(rawCenter)
+      ? rawCenter
+      : rawCenter.toJSON()
+    : null;
+  const lat = center && center.lat;
+  const lng = center && center.lng;
 
   /* eslint-disable react-hooks/exhaustive-deps --
    *
@@ -17,32 +36,48 @@ export function useMapOptions(map: google.maps.Map | null, mapProps: MapProps) {
    */
 
   // update the map options when mapOptions is changed
+  // Note: due to the destructuring above, mapOptions will be seen as changed
+  //   with every re-render, so we're boldly assuming the maps-api will properly
+  //   deal with unchanged option-values passed into setOptions.
   useEffect(() => {
     if (!map) return;
 
-    map.setOptions(mapOptions);
+    // Changing the mapId via setOptions will trigger an error-message.
+    // We will re-create the map-instance in that case anyway, so we
+    // remove it here to avoid this error-message.
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const {mapId, ...opts} = mapOptions;
+    map.setOptions(opts);
   }, [mapOptions]);
 
   useLayoutEffect(() => {
-    if (!map || !center) return;
+    if (!map || !Number.isFinite(lat) || !Number.isFinite(lng)) return;
+    if (
+      cameraStateRef.current.center.lat === lat &&
+      cameraStateRef.current.center.lng === lng
+    )
+      return;
 
-    map.moveCamera({center});
-  }, [center]);
+    map.moveCamera({center: {lat: lat as number, lng: lng as number}});
+  }, [lat, lng]);
 
   useLayoutEffect(() => {
     if (!map || !Number.isFinite(zoom)) return;
+    if (cameraStateRef.current.zoom === zoom) return;
 
     map.moveCamera({zoom: zoom as number});
   }, [zoom]);
 
   useLayoutEffect(() => {
     if (!map || !Number.isFinite(heading)) return;
+    if (cameraStateRef.current.heading === heading) return;
 
     map.moveCamera({heading: heading as number});
   }, [heading]);
 
   useLayoutEffect(() => {
     if (!map || !Number.isFinite(tilt)) return;
+    if (cameraStateRef.current.tilt === tilt) return;
 
     map.moveCamera({tilt: tilt as number});
   }, [tilt]);
