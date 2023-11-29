ee3bba8f577f4c71b0e157af2e3956ca3971e64b
Martin Schuhfuss <m.schuhfuss@gmail.com>
2023-11-29 15:03:27 +0100

    docs: update vite (#113)

---
+++ scripts/update-examples.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#rootDir=`readlink -f "$(dirname $0)/.."`
+rootDir="$(cd -P "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+for d in `find ${rootDir}/examples -type d -depth 1` ; do
+  echo ">>> updating example '$(basename $d)'"
+  (
+    cd $d
+    npm --no-progress --no-audit --no-fund --silent update
+    to_update=`npm outdated --json | jq -r 'to_entries[] | select(.value.wanted != .value.latest) | .key'`
+
+    for pkg in $to_update ; do
+      npm install $pkg@latest
+    done
+  )
+
+done
