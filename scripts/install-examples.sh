6b26043166038ed74ee1038b921de6844edad00d
Malte Modrow <modrow@ubilabs.net>
2024-01-23 15:59:49 +0100

    feat(examples): resolve node_modules of examples in website

---
+++ scripts/install-examples.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+rootDir="$(cd -P "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+for d in `find ${rootDir}/examples -type d -depth 1` ; do
+  echo ">>> installing example '$(basename $d)'"
+  (
+    cd $d
+    npm ci --silent
+  )
+
+done
--- website/scripts/install-external-dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-npm install \
-@googlemaps/markerclusterer \
-deck.gl \
---no-save
