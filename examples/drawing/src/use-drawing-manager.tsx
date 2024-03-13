8d809ea5350e79e6d4f9c8472f0a76147b9183f1
Malte Modrow <modrow@ubilabs.net>
2024-03-13 18:28:08 +0100

    docs: add cleanup function for drawing manager (#262)

--- examples/drawing/src/use-drawing-manager.tsx
+++ examples/drawing/src/use-drawing-manager.tsx
@@ -13,42 +13,46 @@ export function useDrawingManager(
   useEffect(() => {
     if (!map || !drawing) return;
 
-    setDrawingManager(
-      // https://developers.google.com/maps/documentation/javascript/reference/drawing
-      new drawing.DrawingManager({
-        map,
-        drawingMode: google.maps.drawing.OverlayType.CIRCLE,
-        drawingControl: true,
-        drawingControlOptions: {
-          position: google.maps.ControlPosition.TOP_CENTER,
-          drawingModes: [
-            google.maps.drawing.OverlayType.MARKER,
-            google.maps.drawing.OverlayType.CIRCLE,
-            google.maps.drawing.OverlayType.POLYGON,
-            google.maps.drawing.OverlayType.POLYLINE,
-            google.maps.drawing.OverlayType.RECTANGLE
-          ]
-        },
-        markerOptions: {
-          draggable: true
-        },
-        circleOptions: {
-          editable: true
-        },
-        polygonOptions: {
-          editable: true,
-          draggable: true
-        },
-        rectangleOptions: {
-          editable: true,
-          draggable: true
-        },
-        polylineOptions: {
-          editable: true,
-          draggable: true
-        }
-      })
-    );
+    // https://developers.google.com/maps/documentation/javascript/reference/drawing
+    const newDrawingManager = new drawing.DrawingManager({
+      map,
+      drawingMode: google.maps.drawing.OverlayType.CIRCLE,
+      drawingControl: true,
+      drawingControlOptions: {
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: [
+          google.maps.drawing.OverlayType.MARKER,
+          google.maps.drawing.OverlayType.CIRCLE,
+          google.maps.drawing.OverlayType.POLYGON,
+          google.maps.drawing.OverlayType.POLYLINE,
+          google.maps.drawing.OverlayType.RECTANGLE
+        ]
+      },
+      markerOptions: {
+        draggable: true
+      },
+      circleOptions: {
+        editable: true
+      },
+      polygonOptions: {
+        editable: true,
+        draggable: true
+      },
+      rectangleOptions: {
+        editable: true,
+        draggable: true
+      },
+      polylineOptions: {
+        editable: true,
+        draggable: true
+      }
+    });
+
+    setDrawingManager(newDrawingManager);
+
+    return () => {
+      newDrawingManager.setMap(null);
+    };
   }, [drawing, map]);
 
   return drawingManager;
