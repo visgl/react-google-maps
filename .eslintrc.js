b44fbfe19db9edde6fbb60730bbe96dfad407a60
Rodrigo Faria <rodrigo.faria@cartrack.com>
2024-04-03 15:14:53 +0100

    chore: add rule to force devs to memoize context values sent to context providers
    - Avoids severe performance issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,6 +44,7 @@ module.exports = {
         'no-undef': 'off',
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'warn',
+        'react/jsx-no-constructed-context-values': 'error',
 
         // We use function hoisting to put exports at top of file
         '@typescript-eslint/no-use-before-define': 'off',
