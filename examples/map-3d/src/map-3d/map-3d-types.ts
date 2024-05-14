d6b09463f2aecb5ebee65b9b62ff22427a9df088
Martin Schuhfuss <m.schuhfuss@gmail.com>
2024-05-13 18:36:35 -0700

    docs(examples): add new 3d-maps example

---
+++ examples/map-3d/src/map-3d/map-3d-types.ts
@@ -0,0 +1,42 @@
+import {DOMAttributes, RefAttributes} from 'react';
+
+// add an overload signature for the useMapsLibrary hook, so typescript
+// knows what the 'maps3d' library is.
+declare module '@vis.gl/react-google-maps' {
+  export function useMapsLibrary(
+    name: 'maps3d'
+  ): typeof google.maps.maps3d | null;
+}
+
+// add the <gmp-map-3d> custom-element to the JSX.IntrinsicElements
+// interface, so it can be used in jsx
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      ['gmp-map-3d']: CustomElement<
+        google.maps.maps3d.Map3DElement,
+        {
+          [key in GmpMap3DAttributeNames]?: string;
+        }
+      >;
+    }
+  }
+}
+
+type GmpMap3DAttributeNames = keyof Omit<
+  google.maps.maps3d.Map3DElementOptions,
+  'bounds'
+>;
+
+// a helper type for CustomElement definitions
+type CustomElement<TElem, TAttr> = Partial<
+  TAttr &
+    DOMAttributes<TElem> &
+    RefAttributes<TElem> & {
+      // for whatever reason, anything else doesn't work as children
+      // of a custom element, so we allow `any` here
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      children: any;
+    }
+>;
