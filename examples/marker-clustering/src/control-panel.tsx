2bea503d7fb9f7f67b6e1d0d92146a8f8940ec5b
Martin Schuhfuss <m.schuhfuss@gmail.com>
2024-06-14 00:40:23 -0700

    docs(examples): fixed and extended marker-clusterer example (#414)
    The marker-clusterer example included a couple of problems that would
    only surface when being extended in certain ways. These were mostly
    related to missing memoization of callbacks.
    
    This has continued to cause some problems in the community so I
    attempted this version that handles the typical additions like an
    infowindow and dynamic marker updates without problems and in a mostly
    idomatic react way.

--- examples/marker-clustering/src/control-panel.tsx
+++ examples/marker-clustering/src/control-panel.tsx
@@ -1,11 +1,61 @@
-import * as React from 'react';
+import React, {useCallback} from 'react';
 
-function ControlPanel() {
+import {CategoryData} from './trees';
+
+type ControlPanelProps = {
+  categories: Array<CategoryData>;
+  onCategoryChange: (value: string | null) => void;
+};
+
+export const ControlPanel = ({
+  categories,
+  onCategoryChange
+}: ControlPanelProps) => {
+  const handleCategoryChange = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      onCategoryChange(e.target.value || null);
+    },
+    [onCategoryChange]
+  );
   return (
-    <div className="control-panel">
+    <div className="control-panel marker-clustering-control-panel">
       <h3>Marker Clustering</h3>
-      <p>Using MarkerClusterer to cluster Markers on a map.</p>
+      <p>
+        This example uses the <code>@googlemaps/markerclusterer</code> library
+        to demostrate how to render a large dataset of markers on the map.
+      </p>
+      <p>
+        This example also includes a filter function to show dynamic updating of
+        the clustered markers and an InfoWindow to show details about the
+        locations.
+      </p>
+      <p>
+        <label>Filter Trees:</label>{' '}
+        <select onChange={handleCategoryChange}>
+          <option value={''}>All trees</option>
 
+          {categories.map(category => (
+            <option key={category.key} value={category.key}>
+              {category.label} ({category.count})
+            </option>
+          ))}
+        </select>
+      </p>
+      <div className={'attribution'}>
+        <div>
+          <strong>Data:</strong>{' '}
+          <a
+            href="https://open.toronto.ca/dataset/street-tree-data/"
+            target="_blank">
+            Street Tree Data
+          </a>{' '}
+          licensed under the{' '}
+          <a href="https://open.toronto.ca/open-data-license/" target="_blank">
+            Open Government Licence – Toronto
+          </a>
+          .
+        </div>
+      </div>
       <div className="links">
         <a
           href="https://codesandbox.io/s/github/visgl/react-google-maps/tree/main/examples/marker-clustering"
@@ -21,6 +71,4 @@ function ControlPanel() {
       </div>
     </div>
   );
-}
-
-export default React.memo(ControlPanel);
+};
