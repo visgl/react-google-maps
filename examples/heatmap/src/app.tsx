1c365445b5f20a044c49d07998b561022de0c13a
CedricAngeloSina-on <137179865+CedricAngeloSina-on@users.noreply.github.com>
2024-07-09 20:21:23 +0800

    created heatmap component

--- examples/heatmap/src/app.tsx
+++ examples/heatmap/src/app.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {createRoot} from 'react-dom/client';
 
 import {APIProvider, Map} from '@vis.gl/react-google-maps';
 import ControlPanel from './control-panel';
+import Heatmap from './heatmap';
+
+import {FeatureCollection, Point, GeoJsonProperties} from 'geojson';
+import earthquakes from '../data/earthquakes.json';
+
+const earthquakeDataPoints: FeatureCollection<Point, GeoJsonProperties> =
+  earthquakes as FeatureCollection<Point, GeoJsonProperties>;
 
 const API_KEY =
   globalThis.GOOGLE_MAPS_API_KEY ?? (process.env.GOOGLE_MAPS_API_KEY as string);
 
-const App = () => (
-  <APIProvider apiKey={API_KEY}>
-    <Map
-      defaultCenter={{lat: 22.54992, lng: 0}}
-      defaultZoom={3}
-      gestureHandling={'greedy'}
-      disableDefaultUI={true}
-    />
-    <ControlPanel />
-  </APIProvider>
-);
+const App = () => {
+  const [radius, setRadius] = useState(25);
+  const [opacity, setOpacity] = useState(0.8);
+
+  return (
+    <APIProvider apiKey={API_KEY}>
+      <Map
+        defaultCenter={{lat: 40.7749, lng: -130.4194}}
+        defaultZoom={3}
+        gestureHandling={'greedy'}
+        disableDefaultUI={true}
+      />
+      <Heatmap
+        geojson={earthquakeDataPoints}
+        radius={radius}
+        opacity={opacity}
+      />
+      <ControlPanel
+        radius={radius}
+        opacity={opacity}
+        onRadiusChanged={setRadius}
+        onOpacityChanged={setOpacity}
+      />
+    </APIProvider>
+  );
+};
 export default App;
 
 export function renderToDom(container: HTMLElement) {
--- examples/heatmap/src/control-panel.tsx
+++ examples/heatmap/src/control-panel.tsx
@@ -1,6 +1,18 @@
 import * as React from 'react';
 
-function ControlPanel() {
+type Props = {
+  radius: number;
+  opacity: number;
+  onRadiusChanged: (radius: number) => void;
+  onOpacityChanged: (opacity: number) => void;
+};
+
+function ControlPanel({
+  radius,
+  opacity,
+  onRadiusChanged,
+  onOpacityChanged
+}: Props) {
   return (
     <div className="control-panel">
       <h3>Heatmap</h3>
@@ -8,6 +20,43 @@ function ControlPanel() {
         This uses the useMapsLibrary hook showing earthquake magnitude data in a
         heatmap.
       </p>
+
+      {/* Circle Controls */}
+      <div style={{marginBottom: '2rem'}}>
+        <h4>Change the heatmap appearance here:</h4>
+        <div style={{display: 'flex', flexDirection: 'column', gap: '0.5rem'}}>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between'
+            }}>
+            <label htmlFor="radius">Radius:</label>
+            <input
+              type="number"
+              value={radius}
+              onChange={e => onRadiusChanged(Number(e.target.value))}
+              min={5}
+              max={50}
+            />
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between'
+            }}>
+            <label htmlFor="opacity">Opacity:</label>
+            <input
+              type="number"
+              value={opacity}
+              onChange={e => onOpacityChanged(Number(e.target.value))}
+              min={0}
+              max={1}
+              step={0.1}
+            />
+          </div>
+        </div>
+      </div>
+
       <div className="links">
         <a
           href="https://codesandbox.io/s/github/visgl/react-google-maps/tree/main/examples/heatmap"
