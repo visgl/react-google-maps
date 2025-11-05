de1a863342a6b629a7151be922027677fe175158
Rodrigo Faria <rodrigo.faria@cartrack.com>
2025-11-05 09:52:43 +0000

    refactor: improve stability of position and event handlers

--- src/hooks/use-dom-event-listener.ts
+++ src/hooks/use-dom-event-listener.ts
@@ -1,20 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {useEffect} from 'react';
+import {useEffectEvent} from './useEffectEvent';
+
+const noop = () => {};
 
 /**
  * Internally used to bind events to DOM nodes.
  * @internal
  */
 export function useDomEventListener<T extends (...args: any[]) => void>(
-  target?: Node | null,
-  name?: string,
-  callback?: T | null
+  target: Node | null | undefined,
+  name: string,
+  callback: T | null | undefined
 ) {
+  const callbackEvent = useEffectEvent(callback ?? noop);
+  const isCallbackDefined = !!callback;
   useEffect(() => {
-    if (!target || !name || !callback) return;
+    if (!target || !isCallbackDefined) return;
 
-    target.addEventListener(name, callback);
+    // According to react 19 useEffectEvent and our ponyfill, the callback returned by useEffectEvent is NOT stable
+    // Thus, we need to create a stable listener callback that we can then use to removeEventListener with.
+    const listenerCallback: EventListener = (...args) => callbackEvent(...args);
 
-    return () => target.removeEventListener(name, callback);
-  }, [target, name, callback]);
+    target.addEventListener(name, listenerCallback);
+
+    return () => target.removeEventListener(name, listenerCallback);
+  }, [target, name, isCallbackDefined]);
 }
