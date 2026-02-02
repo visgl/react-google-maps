2d27949be0170eefc92f10b102c01e7f2987148f
Martin Schuhfuss <m.schuhfuss@gmail.com>
2026-02-02 10:53:05 +0100

    docs: more explicit comment about disabling dependency check

--- src/components/map-3d/use-map-3d-instance.ts
+++ src/components/map-3d/use-map-3d-instance.ts
@@ -42,59 +42,64 @@ export function useMap3DInstance(
   }, []);
 
   // Apply initial options once when the element is first available
-  useEffect(() => {
-    if (!map3d) return;
+  useEffect(
+    () => {
+      if (!map3d) return;
 
-    const {
-      center,
-      heading,
-      tilt,
-      range,
-      roll,
-      defaultCenter,
-      defaultHeading,
-      defaultTilt,
-      defaultRange,
-      defaultRoll,
-      // Non-element props to exclude
-      id,
-      style,
-      className,
-      children,
-      onCenterChanged,
-      onHeadingChanged,
-      onTiltChanged,
-      onRangeChanged,
-      onRollChanged,
-      onCameraChanged,
-      onClick,
-      onSteadyChange,
-      onAnimationEnd,
-      onError,
-      mode,
-      gestureHandling,
-      ...elementOptions
-    } = props;
+      const {
+        center,
+        heading,
+        tilt,
+        range,
+        roll,
+        defaultCenter,
+        defaultHeading,
+        defaultTilt,
+        defaultRange,
+        defaultRoll,
+        // Non-element props to exclude
+        id,
+        style,
+        className,
+        children,
+        onCenterChanged,
+        onHeadingChanged,
+        onTiltChanged,
+        onRangeChanged,
+        onRollChanged,
+        onCameraChanged,
+        onClick,
+        onSteadyChange,
+        onAnimationEnd,
+        onError,
+        mode,
+        gestureHandling,
+        ...elementOptions
+      } = props;
 
-    const initialCenter = center ?? defaultCenter;
-    const initialHeading = heading ?? defaultHeading;
-    const initialTilt = tilt ?? defaultTilt;
-    const initialRange = range ?? defaultRange;
-    const initialRoll = roll ?? defaultRoll;
+      const initialCenter = center ?? defaultCenter;
+      const initialHeading = heading ?? defaultHeading;
+      const initialTilt = tilt ?? defaultTilt;
+      const initialRange = range ?? defaultRange;
+      const initialRoll = roll ?? defaultRoll;
 
-    const initialOptions: Partial<google.maps.maps3d.Map3DElementOptions> = {
-      ...elementOptions
-    };
+      const initialOptions: Partial<google.maps.maps3d.Map3DElementOptions> = {
+        ...elementOptions
+      };
 
-    if (initialCenter) initialOptions.center = initialCenter;
-    if (initialHeading !== undefined) initialOptions.heading = initialHeading;
-    if (initialTilt !== undefined) initialOptions.tilt = initialTilt;
-    if (initialRange !== undefined) initialOptions.range = initialRange;
-    if (initialRoll !== undefined) initialOptions.roll = initialRoll;
+      if (initialCenter) initialOptions.center = initialCenter;
+      if (initialHeading !== undefined) initialOptions.heading = initialHeading;
+      if (initialTilt !== undefined) initialOptions.tilt = initialTilt;
+      if (initialRange !== undefined) initialOptions.range = initialRange;
+      if (initialRoll !== undefined) initialOptions.roll = initialRoll;
 
-    Object.assign(map3d, initialOptions);
+      Object.assign(map3d, initialOptions);
+    },
+    // this effect should only run when the map3d element first becomes
+    // available, so we skip re-running it when other props change.
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [map3d]); // Only run when map3d element first becomes available
+    [map3d]
+  );
 
   const isReady = !!maps3dLib && customElementReady;
 
