fae7a327df1c8ccd07b2f8275c95dfb10d09605a
Malte Modrow <modrow@ubilabs.net>
2026-06-09 09:50:35 +0200

    feat: cleanup and example images

--- examples/map-3d-routes-widget/vite.config.js
+++ examples/map-3d-routes-widget/vite.config.js
@@ -1,29 +1,16 @@
 import {defineConfig, loadEnv} from 'vite';
-import {resolve} from 'node:path';
 
 export default defineConfig(({mode}) => {
-  const {GOOGLE_MAPS_API_KEY = ''} = loadEnv(mode, resolve('../../'), '');
+  const {GOOGLE_MAPS_API_KEY = ''} = loadEnv(mode, process.cwd(), '');
 
   return {
-    envDir: '../../',
     define: {
       'process.env.GOOGLE_MAPS_API_KEY': JSON.stringify(GOOGLE_MAPS_API_KEY)
     },
     resolve: {
       alias: {
-        '@vis.gl/react-google-maps/examples.js': resolve(
-          '../../website/static/scripts/examples.js'
-        ),
-        '@vis.gl/react-google-maps/examples.css': resolve(
-          '../../examples/examples.css'
-        ),
-        '@vis.gl/react-google-maps': resolve('../../src/index.ts'),
-        '@googlemaps/js-api-loader': resolve(
-          './node_modules/@googlemaps/js-api-loader'
-        ),
-        'fast-deep-equal': resolve('./node_modules/fast-deep-equal'),
-        react: resolve('./node_modules/react'),
-        'react-dom': resolve('./node_modules/react-dom')
+        '@vis.gl/react-google-maps/examples.js':
+          'https://visgl.github.io/react-google-maps/scripts/examples.js'
       }
     }
   };
