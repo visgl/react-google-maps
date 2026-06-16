1404f7935a7e72e15c2f7b0862d6da6486365f5d
Malte Modrow <modrow@ubilabs.net>
2026-06-16 11:19:21 +0200

    refactor: deprecate 3d export from root path

---
+++ src/deprecated-3d-exports.ts
@@ -0,0 +1,114 @@
+import {
+  GestureHandling as GestureHandlingImpl,
+  GoogleMaps3DContext as GoogleMaps3DContextImpl,
+  Map3D as Map3DImpl,
+  MapMode as MapModeImpl
+} from './components/map-3d';
+import type {
+  GestureHandling as GestureHandlingImplType,
+  GoogleMaps3DContextValue as GoogleMaps3DContextValueImpl,
+  Map3DCameraChangedEvent as Map3DCameraChangedEventImpl,
+  Map3DClickEvent as Map3DClickEventImpl,
+  Map3DEvent as Map3DEventImpl,
+  Map3DEventProps as Map3DEventPropsImpl,
+  Map3DProps as Map3DPropsImpl,
+  Map3DRef as Map3DRefImpl,
+  Map3DSteadyChangeEvent as Map3DSteadyChangeEventImpl,
+  MapMode as MapModeImplType
+} from './components/map-3d';
+import {
+  Marker3D as Marker3DImpl,
+  Marker3DContext as Marker3DContextImpl,
+  useMarker3D as useMarker3DImpl
+} from './components/marker-3d';
+import type {
+  Marker3DContextValue as Marker3DContextValueImpl,
+  Marker3DProps as Marker3DPropsImpl
+} from './components/marker-3d';
+import {Popover as PopoverImpl} from './components/popover';
+import type {PopoverProps as PopoverPropsImpl} from './components/popover';
+
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export const GestureHandling = GestureHandlingImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export type GestureHandling = GestureHandlingImplType;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export const GoogleMaps3DContext = GoogleMaps3DContextImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export type GoogleMaps3DContextValue = GoogleMaps3DContextValueImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export const Map3D = Map3DImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export type Map3DProps = Map3DPropsImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export type Map3DRef = Map3DRefImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export const MapMode = MapModeImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export type MapMode = MapModeImplType;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export type Map3DCameraChangedEvent = Map3DCameraChangedEventImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export type Map3DClickEvent = Map3DClickEventImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export type Map3DEvent = Map3DEventImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export type Map3DEventProps = Map3DEventPropsImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export type Map3DSteadyChangeEvent = Map3DSteadyChangeEventImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export const Marker3D = Marker3DImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export const Marker3DContext = Marker3DContextImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export const useMarker3D = useMarker3DImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export type Marker3DContextValue = Marker3DContextValueImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export type Marker3DProps = Marker3DPropsImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export const Popover = PopoverImpl;
+/**
+ * @deprecated Import from `@vis.gl/react-google-maps/3d` instead.
+ */
+export type PopoverProps = PopoverPropsImpl;
