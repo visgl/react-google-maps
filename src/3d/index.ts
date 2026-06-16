9ec2455133ddc92e190964383ffab88fdf0ab729
Malte Modrow <modrow@ubilabs.net>
2026-06-16 11:02:21 +0200

    refactor: better export structure

--- src/3d/index.ts
+++ src/3d/index.ts
@@ -2,4 +2,13 @@
 import type {} from '../../types/react-custom-element-bridge/index';
 export type * from '../../types/react-custom-element-bridge/index';
 
-export * from '../components/3d';
+export * from '../components/3d/circle-path';
+export * from '../components/3d/flattener';
+export * from '../components/3d/marker';
+export * from '../components/3d/model-3d';
+export * from '../components/3d/polygon-3d';
+export * from '../components/3d/polyline-3d';
+export type * from '../components/3d/types';
+export * from '../components/map-3d';
+export * from '../components/marker-3d';
+export * from '../components/popover';
